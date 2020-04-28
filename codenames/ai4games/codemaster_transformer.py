9197d555b8141b52453d0069eca8a9f978e56493
catajara <48599015+catajara@users.noreply.github.com>
2020-04-28 13:05:19 -0400

    Add files via upload
    At least I'm getting a word!! Issue is there are a lot of words in the vocabulary https://raw.githubusercontent.com/huggingface/swift-coreml-transformers/master/Resources/gpt2-vocab.json that are just parts of words, like "\u0120susp": 4012. 
    Also it includes the words with space before "\u0120Another": 6023, and the same word w/o the space before "Another": 6610.
    So we need to think how to solve that...

--- codenames/ai4games/codemaster_transformer.py
+++ codenames/ai4games/codemaster_transformer.py
@@ -12,6 +12,12 @@ import itertools
 import numpy as np
 import random
 import scipy
+import torch
+import torch.nn as nn
+from transformers import AutoModel, AutoModelWithLMHead,  AutoTokenizer, GPT2Tokenizer, TFGPT2Model
+torch.set_grad_enabled(False)
+from sklearn.neighbors import NearestNeighbors
+
 
 
 
@@ -53,11 +59,22 @@ class ai_codemaster(codemaster):
 
 
 		''' WRITE NEW CODE HERE '''
+		# 1. GET EMBEDDING FOR RED WORDS USING GPT2
+
+		tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+		model = AutoModelWithLMHead.from_pretrained("gpt2")
+		
+		def red_embedding(red_words):
+			text_index = tokenizer.encode(red_words,add_prefix_space=True)
+			red_emb = model.transformer.wte.weight[text_index,:]
+			return red_emb
+
+		red_emb = red_embedding(red_words)
 
 
 		# 2. DEFINE THE THRESHOLD (DONE IN THE INITIALIZING FUNCTION)
 
-		# 3. USE THE K NEAREST NEIGHBOR ALGORITHM 
+		# 3. USE THE K NEAREST NEIGHBOR -LIKE ALGORITHM (FIND K NEIGHBORS BASED ON THRESHOLD)
 
 		'''
 
@@ -72,17 +89,56 @@ class ai_codemaster(codemaster):
 			the distance threshold
 
 		'''
+		# cosine similarity
+		def cos_sim(input1, input2):
+		    cos = nn.CosineSimilarity(dim=0,eps=1e-6)
+		    return cos(input1, input2)
+
+		# create distance matrix for words
+		num_words = red_emb.shape[0]
+
+		dist = np.zeros((num_words,num_words))
+		for i in range(num_words):
+		    for j in range((red_emb.shape[0])):
+		        dist[i][j] = cos_sim(red_emb[i],red_emb[j])
+
+		## find the word with more neighbors within threshold
+
+		# count number of neighbors below threshold for each word
+		how_many = []
+		for i in range(num_words):
+		    how_many.append((dist[i] <= dist_threshold).sum())
+
+		#max number of words
+		clue_num = max(how_many)
+
+		# find which is the word with max number of neighbors
+		which = np.where(how_many == clue_num)[0][0]
+
+		# find list of vectors in the subset
+		subset = []
+		subset.append(which)
+		np.where(dist[which] <= dist_threshold)[0]
+		for i in range(np.where(dist[which] <= dist_threshold)[0].shape[0]):
+		    subset.append(np.where(dist[which] <= dist_threshold)[0][i])
+
 
 		# 4. FIND THE CENTROID OF THE SUBSET
+		center = torch.mean(red_emb[subset], dim=0)
 
 		# 5. USE KNN TO FIND THE CLOSEST MATCH IN THE GPT2 MATRIX FOR THE CENTROID VECTOR
+		emb_matrix = model.transformer.wte.weight
+		vectors = emb_matrix.detach()
+		knn = NearestNeighbors(n_neighbors=1)
+		knn.fit(vectors)
+		knn.kneighbors(center.reshape(1,-1))
+
 
 		# 6. RETURN THE WORD FROM THE GPT2 MATRIX + THE NUMBER OF THE NEIGHBORS FROM THE CLUSTER
+		clue = tokenizer.convert_ids_to_tokens(int(knn.kneighbors(center.reshape(1,-1))[1][0][0]))
 
 
-
-		return ["",0]
-
+		return [clue,clue_num]
 
 
 
