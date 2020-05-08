a9b7134ba59fd734f740ffa061531408a9194b07
MasterMilkX <mastermilkx@gmail.com>
2020-05-08 18:38:54 -0400

    Integrated trasnformer to codemaster

--- codenames/ai4games/codemaster_transformer.py
+++ codenames/ai4games/codemaster_transformer.py
@@ -1,7 +1,14 @@
+# TRANSFORMER CODEMASTER
+# CODE BY CATALINA JARAMILLO
+
+
+import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.lancaster import LancasterStemmer
 from nltk.corpus import gutenberg
-import nltk
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+
 from numpy.linalg import norm
 from players.codemaster import codemaster
 from operator import itemgetter
@@ -9,15 +16,18 @@ from numpy import *
 import gensim.models.keyedvectors as word2vec
 import gensim.downloader as api
 import itertools
-import numpy as np
+
 import random
 import scipy
+import re
+
 import torch
 import torch.nn as nn
-from transformers import AutoModel, AutoModelWithLMHead,  AutoTokenizer, GPT2Tokenizer, TFGPT2Model
-torch.set_grad_enabled(False)
+import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
+
+from transformers import AutoModel, AutoModelWithLMHead,  AutoTokenizer, GPT2Tokenizer, TFGPT2Model
 
 
 
@@ -31,6 +41,16 @@ class ai_codemaster(codemaster):
 		self.dist_threshold = 0.3
 
 
+
+		# 1. GET EMBEDDING FOR RED WORDS USING GPT2
+		torch.set_grad_enabled(False)
+		self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+		self.model = AutoModelWithLMHead.from_pretrained("gpt2")
+
+		#get stop words and what-not
+		nltk.download('popular')
+		nltk.download('words')
+		self.corp_words = set(nltk.corpus.words.words())
 
 		return
 
@@ -59,20 +79,14 @@ class ai_codemaster(codemaster):
 
 
 		''' WRITE NEW CODE HERE '''
-		# 1. GET EMBEDDING FOR RED WORDS USING GPT2
-
-		tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-		model = AutoModelWithLMHead.from_pretrained("gpt2")
-		
-		def red_embedding(red_words):
-			text_index = tokenizer.encode(red_words,add_prefix_space=True)
-			red_emb = model.transformer.wte.weight[text_index,:]
-			return red_emb
-
-		red_emb = red_embedding(red_words)
+		# 1. Add \u0120 in front of every word to get a better embedding
+		spec_red_words = list(map(lambda w: "\u0120" + w, red_words))
 
 
-		# 2. DEFINE THE THRESHOLD (DONE IN THE INITIALIZING FUNCTION)
+		print(spec_red_words)
+		# 2. CREATE WORD EMBEDDINGS FOR THE RED WORDS
+		red_emb = self.word_embedding(spec_red_words)  #retrieves embedding for red_words from gpt2 layer 0 (static embedding)
+
 
 		# 3. USE THE K NEAREST NEIGHBOR -LIKE ALGORITHM (FIND K NEIGHBORS BASED ON THRESHOLD)
 
@@ -89,57 +103,122 @@ class ai_codemaster(codemaster):
 			the distance threshold
 
 		'''
-		# cosine similarity
-		def cos_sim(input1, input2):
-		    cos = nn.CosineSimilarity(dim=0,eps=1e-6)
-		    return cos(input1, input2)
+		
 
 		# create distance matrix for words
 		num_words = red_emb.shape[0]
 
 		dist = np.zeros((num_words,num_words))
 		for i in range(num_words):
-		    for j in range((red_emb.shape[0])):
-		        dist[i][j] = cos_sim(red_emb[i],red_emb[j])
+			for j in range((red_emb.shape[0])):
+				dist[i][j] = self.cos_sim(red_emb[i],red_emb[j])
 
 		## find the word with more neighbors within threshold
 
 		# count number of neighbors below threshold for each word
 		how_many = []
 		for i in range(num_words):
-		    how_many.append((dist[i] <= dist_threshold).sum())
+		    how_many.append((dist[i] >= self.dist_threshold).sum())
 
 		#max number of words
 		clue_num = max(how_many)
 
 		# find which is the word with max number of neighbors
-		which = np.where(how_many == clue_num)[0][0]
+		donde = np.where(how_many == clue_num)[0][0]
 
 		# find list of vectors in the subset
 		subset = []
-		subset.append(which)
-		np.where(dist[which] <= dist_threshold)[0]
-		for i in range(np.where(dist[which] <= dist_threshold)[0].shape[0]):
-		    subset.append(np.where(dist[which] <= dist_threshold)[0][i])
+		np.where(dist[donde] >= self.dist_threshold)[0]
+		for i in range(np.where(dist[donde] >= self.dist_threshold)[0].shape[0]):
+		    subset.append(np.where(dist[donde] >= self.dist_threshold)[0][i])
+
+
+		#DEBUG
+		print(subset)
+		#print(red_emb[subset])
+		print(dist)
+		print(how_many)
 
 
 		# 4. FIND THE CENTROID OF THE SUBSET
 		center = torch.mean(red_emb[subset], dim=0)
 
 		# 5. USE KNN TO FIND THE CLOSEST MATCH IN THE GPT2 MATRIX FOR THE CENTROID VECTOR
-		emb_matrix = model.transformer.wte.weight
-		vectors = emb_matrix.detach()
-		knn = NearestNeighbors(n_neighbors=1)
-		knn.fit(vectors)
-		knn.kneighbors(center.reshape(1,-1))
-
+		emb_matrix = self.model.transformer.wte.weight
+		self.vectors = emb_matrix.detach()
+		
 
 		# 6. RETURN THE WORD FROM THE GPT2 MATRIX + THE NUMBER OF THE NEIGHBORS FROM THE CLUSTER
-		clue = tokenizer.convert_ids_to_tokens(int(knn.kneighbors(center.reshape(1,-1))[1][0][0]))
+		clue = self.getBestCleanWord(center, self.words)
+		
+
+		# 6. RETURN THE WORD FROM THE GPT2 MATRIX + THE NUMBER OF THE NEIGHBORS FROM THE CLUSTER
+		#clue = tokenizer.convert_ids_to_tokens(int(knn.kneighbors(center.reshape(1,-1))[1][0][0]))
 
 
 		return [clue,clue_num]
 
+
+	#create word vectors for each word
+	def word_embedding(self, red_words):
+		text_index = self.tokenizer.encode(red_words,add_prefix_space=False)
+		word_emb = self.model.transformer.wte.weight[text_index,:]
+		return word_emb
+
+	# cosine similarity
+	def cos_sim(self, input1, input2):
+		cos = nn.CosineSimilarity(dim=0,eps=1e-6)
+		return cos(input1, input2)
+
+	#clean up the set of words
+	def cleanWords(self, embed):
+		recomm = [i.lower() for i in embed]
+		recomm2 = ' '.join(recomm)
+
+		recomm3 = [w for w in nltk.wordpunct_tokenize(recomm2) \
+		if w.lower() in self.corp_words or not w.isalpha()]
+
+		stop_words = set(stopwords.words('english')) 
+		word_tokens = word_tokenize(' '.join(recomm3)) 
+		recomm4 = [w for w in word_tokens if not w in stop_words]
+
+		excl_ascii = lambda s: re.match('^[\x00-\x7F]+$', s) != None		#checks for ascii only
+		is_uni_char = lambda s: (len(s) == 1) == True						#check if a univode character
+		recomm5 = [w for w in recomm4 if excl_ascii(w) and not is_uni_char(w) and not w.isdigit()]
+
+		return recomm5
+
+	def getBestCleanWord(self, center, board):
+		tries = 1
+		amt = 100
+		maxTry = 5
+
+		knn = NearestNeighbors(n_neighbors=(maxTry*amt))
+		knn.fit(self.vectors)
+		vecinos = knn.kneighbors(center.reshape(1,-1))
+
+		low_board = list(map(lambda w: w.lower(), board))
+
+		while (tries < 5):
+
+			# 6. WORD CLEANUP AND PARSING
+			recomm = []
+			#numrec = (tries-1)*1000
+			for i in range((tries-1)*amt,(tries)*amt):
+				recomm.append(self.tokenizer.decode((int(vecinos[1][0][i])), skip_special_tokens = True, clean_up_tokenization_spaces = True))         
+			clean_words = self.cleanWords(recomm)
+
+			print(clean_words)
+
+			#7. Get the first word not in the board
+			for w in clean_words:
+				if w not in low_board:
+					return w
+
+			#otherwise try again
+			tries+=1
+
+		return "??"		#i got nothing out of 5000 words
 
 
 
